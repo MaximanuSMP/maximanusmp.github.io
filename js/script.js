eb27e861152d03daa328c5e6d83b74f8d6e9ce32
Maximanu <151451667+xXMaximanuXx@users.noreply.github.com>
2025-02-22 10:32:36 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -13,24 +13,12 @@ async function includeHTML() {
 }
 includeHTML();
 
-// Funktion zum Wechseln zwischen Dark und Light Mode
-function toggleDarkMode() {
-    const body = document.body;
-    const currentMode = body.classList.contains('dark-mode') ? 'light' : 'dark';
-    body.classList.toggle('dark-mode', currentMode === 'dark');
-    localStorage.setItem('theme', currentMode); // Speichern der Auswahl im LocalStorage
-}
-
-// Überprüfen, ob ein gespeicherter Modus existiert und anwenden
-document.addEventListener('DOMContentLoaded', () => {
-    const savedTheme = localStorage.getItem('theme');
-    if (savedTheme === 'dark') {
-        document.body.classList.add('dark-mode');
-    }
-
-    // Event Listener für den Dark/Light Mode Schalter im Footer
-    const toggleButton = document.querySelector('#theme-toggle');
-    if (toggleButton) {
-        toggleButton.addEventListener('click', toggleDarkMode);
-    }
-});
+            // IP-Kopieren Funktion
+            document.getElementById("copyIP").addEventListener("click", function () {
+                navigator.clipboard.writeText(serverIP).then(() => {
+                    alert("Server-IP wurde in die Zwischenablage kopiert: " + serverIP);
+                }).catch(err => {
+                    console.error("Fehler beim Kopieren der IP-Adresse:", err);
+                });
+            });
+        });
