f75574a2bebaa6cd540b3b462c562a00d3cfbfd4
Maximanu <151451667+xXMaximanuXx@users.noreply.github.com>
2025-07-30 16:04:52 +0200

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
   // Header & Footer laden
-  fetch('components/header.html')
+  fetch('/components/header.html')
     .then(res => res.text())
     .then(html => document.getElementById('header-placeholder').innerHTML = html);
 
-  fetch('components/footer.html')
+  fetch('/components/footer.html')
     .then(res => res.text())
     .then(html => document.getElementById('footer-placeholder').innerHTML = html);
 
